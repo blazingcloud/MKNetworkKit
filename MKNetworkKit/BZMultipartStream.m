3832c85041953c40f7b200e3242c028e0497d080
Mason <freemason@gmail.com>
2012-07-10 12:33:18 -0700

    cleanup logging

--- MKNetworkKit/BZMultipartStream.m
+++ MKNetworkKit/BZMultipartStream.m
@@ -41,13 +41,15 @@
 
 @implementation BZMultipartStream {
     NSMutableDictionary* properties;
-    
+
     NSStringEncoding encoding;
     NSData* footer;
     NSData* fieldend;
     NSMutableData* fields;
     NSMutableArray* streams;
-    unsigned long sent, index;    
+
+    NSUInteger index;
+    unsigned long sent;
 }
 
 @synthesize boundary, length, streamStatus, streamError;
@@ -55,15 +57,15 @@
 - (id) initWithEncoding:(NSStringEncoding)enc {
     if (self = [super init]) {
         properties = [[NSMutableDictionary alloc] init];
-        
+
         encoding = enc;
         boundary = [[NSProcessInfo processInfo] globallyUniqueString];
-        footer   = [[NSString stringWithFormat:kBZFooterFormat, boundary] dataUsingEncoding:encoding]; 
+        footer   = [[NSString stringWithFormat:kBZFooterFormat, boundary] dataUsingEncoding:encoding];
         streams  = [[NSMutableArray alloc] init];
         fields   = [[NSMutableData alloc] init];
         fieldend = [kBZFieldFooter dataUsingEncoding:encoding];
-        
-        length   = footer.length;        
+
+        length   = footer.length;
         sent     = 0;
         index    = 0;
     }
@@ -77,87 +79,87 @@
         } else {
             [self appendField:value forKey:key];
         }
-    }];            
+    }];
 }
 
 - (void) appendField:(NSData*)value forKey:(NSString*)key {
     NSString* formatted = [NSString stringWithFormat:kBZFieldFormat, boundary, key, value];
-    NSData* encoded = [formatted dataUsingEncoding:encoding];    
+    NSData* encoded = [formatted dataUsingEncoding:encoding];
 
-    [fields appendData:encoded];  
+    [fields appendData:encoded];
     [fields appendData:fieldend];
-    
+
     length += encoded.length + fieldend.length;
 }
 
 - (void) appendData:(NSData*)data forKey:(NSString*)key withMimeType:(NSString*)mime {
     NSString* formatted = [NSString stringWithFormat:kBZDataFormat, boundary, key, key, mime ? mime : kBZDefaultMimeType];
-    NSData* encoded = [formatted dataUsingEncoding:encoding];    
+    NSData* encoded = [formatted dataUsingEncoding:encoding];
 
     [streams addObject:[[NSInputStream alloc] initWithData:encoded]];
     [streams addObject:[[NSInputStream alloc] initWithData:data]];
     [streams addObject:[[NSInputStream alloc] initWithData:fieldend]];
-    
-    length += encoded.length + data.length + fieldend.length;    
+
+    length += encoded.length + data.length + fieldend.length;
 }
 
 - (void) appendFile:(NSString*)path forKey:(NSString*)key withMimeType:(NSString*)mime {
     NSString* formatted = [NSString stringWithFormat:kBZDataFormat, boundary, key, [path lastPathComponent], mime ? mime : [path mimeType]];
-    NSData* encoded = [formatted dataUsingEncoding:encoding];    
+    NSData* encoded = [formatted dataUsingEncoding:encoding];
 
     [streams addObject:[[NSInputStream alloc] initWithData:encoded]];
     [streams addObject:[[NSInputStream alloc] initWithFileAtPath:path]];
     [streams addObject:[[NSInputStream alloc] initWithData:fieldend]];
-    
-    length += encoded.length + [[[NSFileManager defaultManager] attributesOfItemAtPath:path error:NULL] fileSize] + fieldend.length;    
+
+    length += encoded.length + [[[NSFileManager defaultManager] attributesOfItemAtPath:path error:NULL] fileSize] + fieldend.length;
 }
 
 - (NSInputStream*) currentStream {
     if (index > streams.count) {
         @throw @"This probably shouldn't happen";
     }
-    
+
     NSInputStream* stream = [streams objectAtIndex:index];
-    
+
     if (stream.streamStatus == NSStreamStatusNotOpen) {
         [stream open];
     }
-    
+
     if (stream.streamStatus == NSStreamStatusAtEnd) {
-        [stream close];        
+        [stream close];
         index++;
         stream = [self currentStream];
     }
-    
-    return stream;    
+
+    return stream;
 }
 
 - (NSInteger)read:(uint8_t *)buffer maxLength:(NSUInteger)maxlen {
     streamStatus = NSStreamStatusReading;
-        
+
     if (index >= [streams count]) {
         streamStatus = NSStreamStatusAtEnd;
         return 0;
     }
 
     NSInputStream* stream = [self currentStream];
-    
+
     int read = 0;
     while ((read = [stream read:buffer maxLength:maxlen]) == 0) {
         stream = [self currentStream];
     }
-    sent += read;        
-
-    NSLog(@"read %d (from %lu) %lu/%lu diff: %lu", read, index, sent, length, length - sent);
+    sent += read;
+    
+    DLog(@"%@ (read %d bytes on stream %d/%d)", self, read, index, [streams count]);
 
     if (sent >= length) {
         streamStatus = NSStreamStatusAtEnd;
     }
-        
+
     return read;
 }
 
-- (BOOL) setCFClientFlags:(CFOptionFlags)flgs callback:(CFReadStreamClientCallBack)cb context:(CFStreamClientContext*)ctx { return YES; }
+- (BOOL) setCFClientFlags:(CFOptionFlags)flgs callback:(CFReadStreamClientCallBack)cb context:(CFStreamClientContext*)ctx { return NO; }
 - (void) scheduleInCFRunLoop:(CFRunLoopRef)loop forMode:(CFStringRef)mode {}
 - (void) unscheduleFromCFRunLoop:(CFRunLoopRef)loop forMode:(CFStringRef)mode {}
 
@@ -175,18 +177,18 @@
 
 - (BOOL) hasBytesAvailable { return sent < length; }
 - (void) close { streamStatus = NSStreamStatusClosed; }
-- (void) open { 
+- (void) open {
     if (streamStatus) { @throw @"Stream is already open!"; }
-    streamStatus = NSStreamStatusOpening; 
+    streamStatus = NSStreamStatusOpening;
     if (fields.length) {
-        [streams insertObject:[[NSInputStream alloc] initWithData:fields] atIndex:0];        
+        [streams insertObject:[[NSInputStream alloc] initWithData:fields] atIndex:0];
     }
-    [streams addObject:[[NSInputStream alloc] initWithData:footer]]; 
-    streamStatus = NSStreamStatusOpen; 
+    [streams addObject:[[NSInputStream alloc] initWithData:footer]];
+    streamStatus = NSStreamStatusOpen;
 }
 
 - (NSString*) description {
-    return [[NSString alloc] initWithFormat:@"%@ %d/%d bytes", [super description], sent, length];
+    return [[NSString alloc] initWithFormat:@"<%p> %lu/%lu kB", self, sent / 1000, length / 1000];
 }
 
 @end
